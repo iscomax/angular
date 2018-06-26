47e4afc3ce8e5d6dd5b6088e02f9e33d5230a149
iscomax <santamariajc61@gmail.com>
2018-06-26 13:36:56 -0500

    proyecto angular clase

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  selectedHero: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}s
+
+}
